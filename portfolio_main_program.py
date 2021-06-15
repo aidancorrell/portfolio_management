c789a59a252892a8dab59d12144f851b419673f8
aidancorrell <82123712+aidancorrell@users.noreply.github.com>
2021-06-15 13:02:17 -0400

    Update portfolio_main_program.py

--- portfolio_main_program.py
+++ portfolio_main_program.py
@@ -80,12 +80,33 @@ expected_returns = returns_daily.mean()
 # calculating the covariance matrix of the portfolio to determine correlation between tickers
 cov_daily = returns_daily.cov()
 # printing covariance matrix
-print(returns_daily.head())
+print(expected_returns)
 print('')
 print("Covariance Matrix: ")
 print('')
 print(cov_daily)
 print('')
+
+returnsdf = pd.DataFrame(returns_daily)
+returnsdf.columns = symbols
+returnsdf = returnsdf.reset_index()
+#returnsdf.to_csv('daily_returns.csv')
+
+# Gathering index and risk-free rate data for portfolio comparison and Sharpe measure computation
+
+index_data = (web.get_data_yahoo('^GSPC', start_date, end_date)).dropna()
+index_data = pd.DataFrame(index_data)
+index_daily_returns = index_data['Adj Close'].pct_change().dropna()
+index_expected_returns = index_daily_returns.mean()
+
+rf_data = (web.get_data_yahoo('^TNX', start_date, end_date)).dropna()
+rf_data = pd.DataFrame(rf_data)
+rf_daily = rf_data['Adj Close'].pct_change().dropna()
+rf = rf_daily.mean()
+
+#print(index_expected_returns)
+
+##########################
 
 # calculating the standard deviation of each ticker in the portfolio
 single_asset_std = np.sqrt(np.diagonal(cov_daily))
@@ -122,8 +143,41 @@ mrp = portfoliosdf.iloc[0] # maximum risk portfolio(most risky)
 # locating the corner portfolios with the closest expected return to the user desired expected return
 chosen_portfolios = portfoliosdf.iloc[(portfoliosdf['Expected Return']-user_er_daily).abs().argsort()[:2]]
 chosen_portfolios = chosen_portfolios.reset_index()
-print(chosen_portfolios)
 
+# creating a new dataset to compare the chosen corner portfolios with a market index
+index_chosen_port = chosen_portfolios.copy(deep=True)
+index_chosen_port.loc[len(index_chosen_port.index)] = ['101', 'index', 0, 0,0,0,0,0, index_expected_returns, 1/252]
+index_chosen_port['Names'] = ['Optimal Portfolio 1', 'Optimal Portfolio 2', 'S&P 500 Index']
+index_chosen_port['Expected Returns Monthly'] = (index_chosen_port['Expected Return']*252)/12
+index_chosen_port['Sharpe Ratio'] = ((index_chosen_port['Expected Return'] - rf) / (index_chosen_port['Risk']))*252/12
+index_chosen_port.to_csv('index_chosen_port.csv')
+print(chosen_portfolios)
+print(index_chosen_port.head())
+
+### work in progress ###
+
+"""
+ticker1_weight = [chosen_portfolios.iloc[0][ticker1]]
+ticker2_weight = [chosen_portfolios.iloc[0][ticker2]]
+ticker3_weight = [chosen_portfolios.iloc[0][ticker3]]
+ticker4_weight = [chosen_portfolios.iloc[0][ticker4]]
+ticker5_weight = [chosen_portfolios.iloc[0][ticker5]]
+ticker6_weight = [chosen_portfolios.iloc[0][ticker6]]
+#print(ticker1_weight)
+
+chosen_returns = pd.DataFrame()
+chosen_returns['Date'] = returnsdf['Date']
+chosen_returns['optimal1'] = returnsdf[ticker1] * ticker1_weight
+chosen_returns['optimal2'] = returnsdf[ticker2] * ticker2_weight
+chosen_returns['optimal3'] = returnsdf[ticker3] * ticker3_weight
+chosen_returns['optimal4'] = returnsdf[ticker4] * ticker4_weight
+chosen_returns['optimal5'] = returnsdf[ticker5] * ticker5_weight
+chosen_returns['optimal6'] = returnsdf[ticker6] * ticker6_weight
+chosen_returns['daily_return'] = chosen_returns['optimal1'] + chosen_returns['optimal2'] + chosen_returns['optimal3'] + chosen_returns['optimal4'] + chosen_returns['optimal5'] + chosen_returns['optimal6']
+
+#chosen_returns.to_csv('chosen_returns.csv')
+"""
+################
 
 # printing optimal portfolios dataframe to a CSV file for reference
 portfoliosdf.to_csv('markowitz_portfolios.csv')
@@ -140,13 +194,17 @@ plt.title('Efficient Frontier', fontsize=24)
 
 plt.show()
 
-#### WORK IN PROGRESS #### visualize first ticker's daily returns over the series
+#### WORK IN PROGRESS #### visualize optimal portfolios vs index
+plt.figure(figsize=(8,4))
+sns.barplot(data=index_chosen_port, x='Names', y='Expected Returns Monthly', palette=['limegreen', 'turquoise', 'mediumpurple'], alpha=0.85)
+plt.title('Optimal Portfolios Returns compared to Market Index')
+plt.ylabel('Expected Returns(Monthly)',fontsize=12)
+plt.xlabel('')
+plt.show()
 
-returnsdf = pd.DataFrame(returns_daily)
-returnsdf.columns = symbols
-returnsdf = returnsdf.reset_index()
-#returnsdf.to_csv('daily_returns.csv')
 
+
+### visualizing an individual ticker's returns with the returns of the entire efficient frontier
 
 plt.figure(figsize=(12,8))
 
@@ -166,6 +224,8 @@ plt.show()
 
 #####
 
+
+# plotting the distribution of returns of each ticker in the portfolio
 
 plt.figure(figsize= (14,8))
 
@@ -195,7 +255,12 @@ plt.title('Distribution of returns: ' + str(ticker6), fontsize = 14)
 
 plt.subplots_adjust(hspace=0.5)
 
-#sns.displot(returnsdf[ticker1], kind='kde', color='g')
-#sns.displot(returnsdf[ticker1], kind='hist', color='g')
 
 plt.show()
+
+#plt.figure(figsize=(14, 8))
+#sns.barplot(data=chosen_returns, x="Date", y="daily_return", color = 'b')
+#sns.barplot(data=returnsdf, x="Date", y=ticker1, alpha=0.6, color = 'r')
+#plt.title('Daily Returns of Optimal Chosen Portfolio vs ' + str(ticker1), fontsize = 14)
+#plt.legend()
+#plt.show()
